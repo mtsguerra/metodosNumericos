2b91bef30abcc3d0aee505fa828d59861fc1f4dc
Matheus Guerra <mtsguerra9@gmail.com>
2025-10-08 16:13:50 +0100

    Finishing ex2

--- exercicio2.java
+++ exercicio2.java
@@ -2,39 +2,42 @@ import java.util.*;
 
 class exercicio2{
 
-    private double factorial(int n) {
-        if (n == 0 || n == 1) return 1.0;
-        return n * factorial(n - 1);
-    }
+    public double calcS(double eps) {
+        double current = 0.5;
+        double acc = current;
+        int k = 0;
+        int n = 1;
 
-    private double calcCurrent(int k){
-        return (factorial(2 * k)
-                / (Math.pow(4, k) * Math.pow(factorial(k), 2) * (2 * k + 1)))
-                * Math.pow(0.5, 2 * k + 1);
-    }
+        while (true) {
+            double numerator   = (2.0 * k + 1.0) * (2.0 * k + 1.0) * 0.25;
+            double denominator = 2.0 * (k + 1.0) * (2.0 * k + 3.0);
+            double next_one = current * (numerator / denominator);
 
-    private void caclS(double eps) {
-        double S = 0.0;
-        int k = 1;
-        double current;
-        current = calcCurrent(0);
-        S += current;
-        while (Math.abs(current) > eps) {
-            current = calcCurrent(k);
-            S += current;
+            if (8.0 * next_one <= eps) {
+                break;
+            }
+
+            acc += next_one;
+            current = next_one;
             k++;
+            n++;
         }
-        S *= 6;
+        double S = 6.0 * acc;
 
-        System.out.println("epsilon: " + eps);
+        System.out.println("Número de termos somados: " + n);
         System.out.println("S: " + S);
+
+        System.out.printf(Locale.US,
+                "epsilon = %.1e | S ≈ %.17f | erro abs ≈ %.3e%n",
+                eps, S, Math.abs(Math.PI - S));  // S = π teoricamente
+        return S;
     }
 
     public static void main(String[] args) {
         exercicio2 obj = new exercicio2();
         double[] epsilons = {1e-5, 1e-10, 1e-15};
         for (double eps : epsilons) {
-            obj.caclS(eps);
+            obj.calcS(eps);
         }
     }
 
